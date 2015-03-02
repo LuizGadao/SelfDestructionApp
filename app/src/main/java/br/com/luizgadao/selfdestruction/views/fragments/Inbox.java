bf24cfd367ae53673595b79463fb53221dc1f5ce
LuizGadao <luizgadao@gmail.com>
2015-03-02 08:01:21 -0300

    adjustments

--- app/src/main/java/br/com/luizgadao/selfdestruction/views/fragments/Inbox.java
+++ app/src/main/java/br/com/luizgadao/selfdestruction/views/fragments/Inbox.java
@@ -5,18 +5,60 @@ import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 import br.com.luizgadao.selfdestruction.R;
+import br.com.luizgadao.selfdestruction.utils.ParseConstants;
 
 /**
  * Created by luizcarlos on 25/02/15.
  */
 public class Inbox extends ListFragment {
 
+    private List<ParseObject> mMessages;
+
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
         View rootView = inflater.inflate( R.layout.fragment_inbox, container, false );
         
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>( ParseConstants.CLASS_MESSAGE );
+        query.whereEqualTo( ParseConstants.KEY_FRIENDS_RELATION, ParseUser.getCurrentUser().getObjectId() );
+        query.orderByAscending( ParseConstants.KEY_CREATED_AT );
+        query.findInBackground( new FindCallback<ParseObject>() {
+            @Override
+            public void done( List<ParseObject> messages, ParseException e ) {
+                if ( e == null )
+                {
+                    //found messages
+                    mMessages = messages;
+                    String[] userName = new String[mMessages.size()];
+                    int i = 0;
+                    for ( ParseObject message : messages )
+                    {
+                        userName[i] = message.getString( ParseConstants.KEY_SENDER_NAME );
+                        i++;
+                    }
+
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>( getActivity(),
+                            android.R.layout.simple_list_item_1, userName );
+                    setListAdapter(adapter);
+                }
+            }
+        } );
     }
 }
